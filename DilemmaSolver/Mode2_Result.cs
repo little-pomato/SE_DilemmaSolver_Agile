bf4fb2d7116dc619ffef6de87c4e0022f1157735
little-pomato <unknownteseng@gmail.com>
2025-11-06 12:03:54 +0800

    Rename project and solution from MoodPicker to DilemmaSolver

---
+++ DilemmaSolver/Mode2_Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DilemmaSolver
+{
+    public partial class Mode2_Result: UserControl
+    {
+        public Mode2_Result()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
